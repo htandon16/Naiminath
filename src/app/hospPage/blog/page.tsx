a28735b2e9f9c013f975e36c279eecbc1c128500
name <email>
2023-12-04 10:30:52 -0800

    latest

--- src/app/hospPage/blog/page.tsx
+++ src/app/hospPage/blog/page.tsx
@@ -12,6 +12,7 @@ interface Blog {
 
 const BlogPage = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
+  const [loading, setLoading] = useState(true); // Added loading state
   const { blogData, setBlogData } = useGlobalContext();
   const router = useRouter();
   
@@ -28,58 +29,69 @@ const BlogPage = () => {
       return data;
     } catch (error: any) {
       console.error("Error fetching data:", error.message);
+      throw error; // Rethrow the error to handle it elsewhere if needed
     }
   };
 
   useEffect(() => {
-    fetchBlogsApi().then((result) => {
-      setBlogs(result);
-      console.log("getResult=", result);
-    });
+    fetchBlogsApi()
+      .then((result) => {
+        setBlogs(result);
+        setLoading(false); // Set loading to false when data is fetched
+        console.log("getResult=", result);
+      })
+      .catch(() => {
+        setLoading(false); // Set loading to false on error
+      });
   }, []);
  
   return (
     <div className="min-h-screen flex flex-wrap gap-5 p-[20px] justify-center items-center">
-      {blogs.map((blog, index) => (
-        <div
-          key={blog.fileName}
-          className="flex flex-col justify-center items-center basis-1/4 h-fit rounded-lg 
+      {loading ? ( // Show loader if still loading
+        <div className="text-center text-black">Loading...</div>
+      ) : (
+        blogs.map((blog, index) => (
+          <div
+            key={blog.fileName}
+            className="flex flex-col justify-center items-center basis-1/4 h-fit rounded-lg 
           overflow-hidden min-h-[350px] shadow-md inset-11 hover:bg-slate-50 cursor-pointer"
-          onClick={async () => {
-            setBlogData(blog.content);
-            // router.push(`/hospPage/blog/blog-detail?data=${blog.content}`);
-            router.push(`/hospPage/blog/blog-detail`);
-          }}
-        >
-          <div className="w-full pb-2 flex-1 justify-center items-center">
-            <h1 className="text-white py-2 text-center bg-black ">
-              Blog {"-"} {index + 1}
-            </h1>
+            onClick={async () => {
+              setBlogData(blog.content);
+              // router.push(`/hospPage/blog/blog-detail?data=${blog.content}`);
+              router.push(`/hospPage/blog/blog-detail`);
+            }}
+          >
+            <div className="w-full pb-2 flex-1 justify-center items-center">
+              <h1 className="text-white py-2 text-center bg-black ">
+                Blog {"-"} {index + 1}
+              </h1>
+              <div
+                className=" overflow-hidden px-2 pt-2 w-full"
+                style={{
+                  display: "-webkit-box",
+                  WebkitBoxOrient: "vertical",
+                  overflow: "hidden",
+                  WebkitLineClamp: 10,
+                }}
+                dangerouslySetInnerHTML={{ __html: blog.content }}
+              />
+            </div>
             <div
-              className=" overflow-hidden px-2 pt-2 w-full"
+              className="text-white overflow-hidden w-full p-4 text-center  bg-slate-400"
               style={{
                 display: "-webkit-box",
                 WebkitBoxOrient: "vertical",
                 overflow: "hidden",
-                WebkitLineClamp: 10,
+                WebkitLineClamp: 2,
               }}
-              dangerouslySetInnerHTML={{ __html: blog.content }}
-            />
+            >
+              {/* {`${blog.fileName}`} */}
+            </div>
           </div>
-          <div
-            className="text-white overflow-hidden w-full p-4 text-center  bg-slate-400"
-            style={{
-              display: "-webkit-box",
-              WebkitBoxOrient: "vertical",
-              overflow: "hidden",
-              WebkitLineClamp: 2,
-            }}
-          >
-            {/* {`${blog.fileName}`} */}
-          </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
+
 export default BlogPage;
