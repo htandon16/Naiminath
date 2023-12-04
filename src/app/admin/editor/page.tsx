998c73f2bd4c81f36e98070e11fee8086512a9d6
name <email>
2023-12-04 01:26:38 -0800

    latest

--- src/app/admin/editor/page.tsx
+++ src/app/admin/editor/page.tsx
@@ -1,57 +1,32 @@
 "use client";
 import React, { useRef, useState } from "react";
-import ReactQuill, { Quill } from "react-quill";
+import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import QuillToolbar, {
-  formats,
-  modules,
-} from "@/app/(components)/QuillEditorToolbar";
+import QuillToolbar, { formats, modules } from "@/app/(components)/QuillEditorToolbar";
 
 const QuillEditor = () => {
   const [content, setContent] = useState({ value: "" });
-  const quillRef = useRef<ReactQuill>(null);
-  //const turndown = new TurndownService();
 
   const handleChange = (value: string) => {
     setContent({ value });
   };
 
   const handleUpload = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    e.preventDefault;
+    e.preventDefault();
 
     if (typeof document !== 'undefined') {
-      
       var myEditor = document.querySelector(".ql-editor");
-    var htmlContent = myEditor?.innerHTML || "";
-    console.log("Content:querySelector==", htmlContent);
+      var htmlContent = myEditor?.innerHTML || "";
+      console.log("Content:querySelector==", htmlContent);
 
-    const writeHtmlFileApi = async () => {
-      await fetch("/api/ncism", {
-        method: "POST",
-        body: JSON.stringify({ content: htmlContent }),
-      });
-    };
-    writeHtmlFileApi();
-   }
-    
-
-    // const fileName = `dynamicFile_${Date.now()}.html`;
-
-    // const filePath = `../../admin/blogfiles/${fileName}`;
-    // console.log("HTML string has been written to the file:", filePath);
-    // fs.writeFileSync(filePath, htmlContent, "utf-8");
-
-    // console.log("HTML string has been written to the file:", filePath);
-
-    //convertHtmlToMdx(htmlContent);
-    //const mdxContent = turndown.turndown(htmlContent);
-    //console.log("Content:mdxContent==", mdxContent);
-    // if (quillRef.current) {
-    //   const editor = quillRef.current.getEditor();
-    //   const html = editor.root.innerHTML;
-    //   console.log("Content:quillRef==", html);
-
-    // }
+      const writeHtmlFileApi = async () => {
+        await fetch("/api/ncism", {
+          method: "POST",
+          body: JSON.stringify({ content: htmlContent }),
+        });
+      };
+      writeHtmlFileApi();
+    }
   };
 
   return (
@@ -65,10 +40,9 @@ const QuillEditor = () => {
           placeholder="Write something awesome..."
           modules={modules}
           formats={formats}
-          //ref={quillRef}
         />
         <button
-          className=" text-white bg-teal-400 py-2 px-3 rounded-lg mt-10 text-center align-middle block mx-auto"
+          className="text-white bg-teal-400 py-2 px-3 rounded-lg mt-10 text-center align-middle block mx-auto"
           onClick={(e) => handleUpload(e)}
         >
           Upload
@@ -77,13 +51,5 @@ const QuillEditor = () => {
     </div>
   );
 };
-
-async function convertHtmlToMdx(htmlString: string) {
-  // Parser.parse(htmlString, { contentType: "markdown" }).then((result: any) =>
-  //   console.log(result)
-  // );
-  // const markdownString = quillToMarkdown(htmlString);
-  // console.log("Content:querySelector==", markdownString);
-}
 
 export default QuillEditor;
