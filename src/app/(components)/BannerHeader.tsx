07c6a159cb75477163524597c22d8546f4033ffa
name <email>
2023-12-03 23:50:36 -0800

    latest code

--- src/app/(components)/BannerHeader.tsx
+++ src/app/(components)/BannerHeader.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Header from "./Header";
 import ImageSlider from "./ImageSlider";
-import "flowbite";
 import { useGlobalContext } from "../context/store";
 
 const BannerHeader = () => {
