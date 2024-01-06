620e84fe834feeda75f6ebb7cec9c3b8e2294251
name <email>
2024-01-06 02:25:42 -0800

    latest

--- src/app/(components)/ImportantLink.tsx
+++ src/app/(components)/ImportantLink.tsx
@@ -24,7 +24,7 @@ export const ImportantLink = () => {
 
       {isPopupVisible && (
         <div
-          className="fixed bottom-[25vh] right-12 bg-[#ded636] text-black px-4 py-2 rounded shadow"
+          className="fixed bottom-[25vh] right-12 bg-[#ded636] text-[#000000] px-4 py-2 rounded shadow"
           onMouseEnter={showPopup}
           onMouseLeave={hidePopup}
         >
@@ -35,46 +35,46 @@ export const ImportantLink = () => {
             </p>
             <p>
               1. Ministry of AYUSH (
-              <a href="https://main.ayush.gov.in/">
+              <a href="https://main.ayush.gov.in/" className=" text-[#1e73be]  underline">
                 https://main.ayush.gov.in/
               </a>
               )
             </p>
             <p>
               2. National Commission for Indian System of Medicine (
-              <a href="https://ncismindia.org/">https://ncismindia.org/</a>)
+              <a href="https://ncismindia.org/" className=" text-[#1e73be]  underline">https://ncismindia.org/</a>)
             </p>
             <p>
               3. Dr. Bhimrao Ambedkar University, Agra (
-              <a href="http://www.dbrau.org.in/">http://www.dbrau.org.in/</a>)
+              <a href="http://www.dbrau.org.in/" className=" text-[#1e73be]  underline">http://www.dbrau.org.in/</a>)
             </p>
             <p>
               4. Mahayogi Guru Gorakhnath AYUSH University, Uttar Pradesh (
-              <a href="https://www.mggaugkp.ac.in/">www.mggaugkp.ac.in</a>
+              <a href="https://www.mggaugkp.ac.in/" className=" text-[#1e73be]  underline">www.mggaugkp.ac.in</a>
               )&nbsp;
             </p>
             <p>
               5. Uttar Pradesh PG AYUSH Counselling (
-              <a href="https://pgcounselling.ayushup.in/)">
+              <a href="https://pgcounselling.ayushup.in/" className=" text-[#1e73be]  underline">
                 http://pgcounselling.ayushup.in/)
               </a>
             </p>
             <p>
               6. Uttar Pradesh State AYUSH Society (
-              <a href="https://web.archive.org/web/20230922012956/https://ayushup.in/">
+              <a href="https://web.archive.org/web/20230922012956/https://ayushup.in/" className=" text-[#1e73be]  underline">
                 https://ayushup.in/
               </a>
               )
             </p>
             <p>
               7. Vidya Lakshmi Education Loan Portal (
-              <a href="https://web.archive.org/web/20230922012956/https://www.vidyalakshmi.co.in/Students/">
+              <a href="https://web.archive.org/web/20230922012956/https://www.vidyalakshmi.co.in/Students/" className=" text-[#1e73be]  underline">
                 https://www.vidyalakshmi.co.in/Students/
               </a>
               )
             </p>
             <p>
-              <a href="https://forms.eduqfix.com/nhmcharcof/add">
+              <a href="https://forms.eduqfix.com/nhmcharcof/add" className=" font-bold">
                8. FEES PAYMENT: CLICK HERE TO PAY FEES
               </a>
             </p>
