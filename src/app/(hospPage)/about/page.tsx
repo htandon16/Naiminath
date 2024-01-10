538b847f4c1f98d49e2eddde9198c64055f86160
name <email>
2024-01-09 22:34:05 -0800

    latest

--- src/app/(hospPage)/about/page.tsx
+++ src/app/(hospPage)/about/page.tsx
@@ -23,14 +23,14 @@ const AboutUs = () => {
       <div className="flex flex-col items-center justify-center font-sans h-full w-full">
         <div
           className=" bg-[url('/images/aboutus_bg.jpg')] bg-cover h-[20vh] w-full flex items-center justify-center 
-      text-[#54595f] text-[32px] font-extrabold border-b-[0.009px] border-[#8b939465]"
+      text-[#54595f] md:text-[32px] text-[28px] font-extrabold border-b-[0.009px] border-[#8b939465]"
         >
           About Us
         </div>
-        <div className="flex flex-row w-[74vw] justify-center gap-6 mt-10">
+        <div className="flex md:flex-row flex-col w-[90vw] md:w-[74vw] justify-center gap-6 mt-10">
           <Image
             src="/images/about_hospital.jpg"
-            className="w-[390px] h-[290px] rounded-[7px] shadow-md ring-1 ring-slate-100"
+            className="w-[390px] h-[240px] md:h-[290px] rounded-[7px] shadow-md ring-1 ring-slate-100"
             alt="about image"
             width={1600}
             height={800}
@@ -55,19 +55,21 @@ const AboutUs = () => {
             </p>
           </div>
         </div>
-        <div className="relative mt-20 w-full h-[80vh] bg-orange-700 flex justify-center items-center">
+        <div className="relative mt-10 md:mt-20 w-full md:h-[80vh] h-[100vh] bg-orange-700 flex justify-center items-center overflow-hidden ">
           <Image
             src="/images/abou_us_green_tea_bg.jpg"
             className="w-full h-full rounded-[7px] object-cover"
             alt="about image"
             layout="fill"
           />
-          <div className="absolute top-1/4 w-[75vw] h-full flex flex-row justify-center items-start bg-opacity-70 gap-10">
-            <div className=" basis-1/2">
-              <div className="text-[#3c4f43] text-[32px] font-medium">
+          <div className="absolute top-2 md:top-1/4 w-[90vw] md:w-[75vw] md:h-full flex md:flex-row flex-col
+           justify-center items-start bg-opacity-70 gap-2 md:gap-10">
+
+            <div className=" md:basis-1/2 ">
+              <div className="text-[#3c4f43] md:text-[32px] text-[26px] font-medium">
                 Vision
               </div>
-              <p className="mt-3 text-[#506759]">
+              <p className="md:mt-3 text-[#506759]">
                 Our vision is to be an innovative, leading global Ayurvedic
                 Medical College, Hospital and Research Centre dedicated to
                 transforming the youth into the most competent doctors equipped
@@ -78,23 +80,23 @@ const AboutUs = () => {
               </p>
             </div>
             <div
-              className=" basis-1/2 text-white"
-              style={{ textShadow: "1px 1px 2px #0005b6" }}
+              className=" md:basis-1/2 text-white"
+              style={{ textShadow: "1px 1px 2px #0005b6" }} 
             >
-              <div className="text-[30px] font-medium">Mission</div>
-              <p className="mt-3">
+              <div className="md:text-[30px] text-[24px] font-medium">Mission</div>
+              <p className="md:mt-3">
                 The mission of Naiminath Ayurvedic Hospital is to improve the
                 health of our Community and the Global Community by setting the
                 standard of excellence in patient care. We aim –
               </p>
-              <p className="mt-3">
+              <p className="md:mt-3">
                 To be the world’s preeminent Ayurvedic healthcare institution.
               </p>
-              <p className="mt-3">
+              <p className="md:mt-3">
                 To educate budding healthcare professionals and to participate
                 in appropriate clinical research.
               </p>
-              <p className="mt-3">
+              <p className="md:mt-3 ">
                 To cultivate an environment of trust, honesty, mutual respect
                 and compassion.
               </p>
