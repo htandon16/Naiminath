22ead8e2a2448be47307d484cec391265aa6e1a2
htandon16 <122512848+htandon16@users.noreply.github.com>
2024-02-16 20:42:35 +0530

    Update page.tsx

--- src/app/(hospPage)/about/page.tsx
+++ src/app/(hospPage)/about/page.tsx
@@ -56,7 +56,8 @@ const AboutUs = () => {
             </p>
                    </div>
         </div>
-            <p className=" mt-3 ml-250">
+<div className="w-[95%] md:w-[85%] h-full mt-[3%] md:mx-[10%] mx-[5%] py-[2%]"   
+              <p className=" mt-3 ml-250">
               What sets Naiminath apart from other private ayurvedic colleges in
               UP is its emphasis on practical experience alongside theoretical
               knowledge. This prestigious ayurvedic medical college offers a
@@ -74,15 +75,18 @@ const AboutUs = () => {
               panchkarma therapy rooms to suit the need and affordability of one
               and all.
             </p> 
-<h3 className=" text-[30px] font-semibold">  Sustainable, Scalable and Replicability of Operations</h3>
-          <p className="mt-3">
-              Through years of practice and practical learnings we have developed detailed SOPs for each and every department which helps us in ramping up our staff on a faster and precise manner. A holistic periodic training curriculum is available for continuity of learning and upgrading our knowledge base. Training processes have been bifurcated for Clinical, Non-Clinical and Physicians separately for clear understanding of job roles. Regular knowledge transfer sessions are conducted to enable cross training and intra-operability of employees. 
-            </p>
-<div className=" text-[30px] font-semibold"> Affordability and Accessibility </div>
-          <p className="mt-3">
-              The institute believes in the principles of healthcare for all and hence our price points have been fixed at a minimal rate based on the general public economic status in the city. Naiminath Ayurvedic Hospital has adopted 5 neighbouring villages and provide free of cost treatment to them all. Regular free camps are conducted for patients who cannot afford quality healthcare in the city and are treated here with exceptional results.
-          </p>
+
+          <h3 className=" text-[30px] font-semibold">  Sustainable, Scalable and Replicability of Operations</h3>
+                    <p className="mt-3">
+                        Through years of practice and practical learnings we have developed detailed SOPs for each and every department which helps us in ramping up our staff on a faster and precise manner. A holistic periodic training curriculum is available for continuity of learning and upgrading our knowledge base. Training processes have been bifurcated for Clinical, Non-Clinical and Physicians separately for clear understanding of job roles. Regular knowledge transfer sessions are conducted to enable cross training and intra-operability of employees. 
+                      </p>
+          <div className=" text-[30px] font-semibold"> Affordability and Accessibility </div>
+                    <p className="mt-3">
+                        The institute believes in the principles of healthcare for all and hence our price points have been fixed at a minimal rate based on the general public economic status in the city. Naiminath Ayurvedic Hospital has adopted 5 neighbouring villages and provide free of cost treatment to them all. Regular free camps are conducted for patients who cannot afford quality healthcare in the city and are treated here with exceptional results.
+                    </p>
    
+</div>
+
         <div className="relative mt-5 md:mt-12 w-full md:h-[70vh] h-[90vh] bg-orange-700 flex justify-center items-center overflow-hidden ">
           <Image
             src="/images/abou_us_green_tea_bg-overlay (3).webp"
