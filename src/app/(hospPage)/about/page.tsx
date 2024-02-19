e0f7c03eca8b9bf817fae88fcda59b58fefc082f
htandon16 <122512848+htandon16@users.noreply.github.com>
2024-02-19 15:15:19 +0530

    Update page.tsx

--- src/app/(hospPage)/about/page.tsx
+++ src/app/(hospPage)/about/page.tsx
@@ -25,10 +25,10 @@ const AboutUs = () => {
           About Us
         </div>
         <div className="h-screen 
-bg-[url(/images/about_hospital.jpg)] 
+bg-[url(/images/About_naiminath_hospital.webp)] bg-center
 bg-no-repeat  flex md:flex-row flex-col w-[90vw] md:w-[74vw] justify-center gap-6 mt-10 ">
           <Image
-            src="/images/about_hospital.jpg"
+            src="/images/About_naiminath_hospital.webp"
             className="w-[390px] h-[240px] md:h-[290px] rounded-[7px] shadow-md ring-1 ring-slate-100"
             alt="about image"
             width={1600}
