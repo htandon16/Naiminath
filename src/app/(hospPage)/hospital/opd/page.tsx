9f4c039ad199f71c77da19bde39201c0eeb66a25
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-03-30 11:02:45 +0530

    Update page.tsx

--- src/app/(hospPage)/hospital/opd/page.tsx
+++ src/app/(hospPage)/hospital/opd/page.tsx
@@ -150,10 +150,10 @@ const Hospital = ({
           </p>
           <br />
 
-           <div className="flex flex-wrap flex-row flex-4 gap-4 justify-center mx-2 md:mx-10 my-10">
+           <div className="flex flex-wrap flex-row flex-2 gap-4 justify-center mx-2 md:mx-10 my-10">
         
     <img className="rounded-md w-auto aspect-auto max-h-60"  src="/images/opd/panchkarma-patients1.jpeg" alt="Panchkarma Patients" />
-           <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients2.jpeg" alt="Panchkarma Patients" />
+           <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients5.jpeg" alt="Panchkarma Patients" />
 
              </div>
 
@@ -161,7 +161,7 @@ const Hospital = ({
         
     <img className="rounded-md w-auto aspect-auto max-h-60"  src="/images/opd/panchkarma-patients3.jpeg" alt="Panchkarma Patients" />
            <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients4.jpeg" alt="Panchkarma Patients" />
-    <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients5.jpeg" alt="Panchkarma Patients" />
+    <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients2.jpeg" alt="Panchkarma Patients" />
 
              </div>
          
