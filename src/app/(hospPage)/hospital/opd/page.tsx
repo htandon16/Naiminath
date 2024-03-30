ab49df8f502b40fd62765a978a44c5eaf44c6790
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-03-30 11:21:00 +0530

    Update page.tsx

--- src/app/(hospPage)/hospital/opd/page.tsx
+++ src/app/(hospPage)/hospital/opd/page.tsx
@@ -158,13 +158,11 @@ const Hospital = ({
              </div>
           
 
- <div className="flex flex-wrap flex-row flex-4 gap-1 justify-between mx-2 md:mx-10 my-10">
-        
-    <img className="rounded-md w-auto aspect-auto max-h-60"  src="/images/opd/panchkarma-patients3.jpeg" alt="Panchkarma Patients" />
-           <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients4.jpeg" alt="Panchkarma Patients" />
+<div className="flex flex-row gap-1 justify-between mx-2 md:mx-10 my-10">
+    <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients3.jpeg" alt="Panchkarma Patients" />
+    <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients4.jpeg" alt="Panchkarma Patients" />
     <img className="rounded-md w-auto aspect-auto max-h-60" src="/images/opd/panchkarma-patients2.jpeg" alt="Panchkarma Patients" />
-
-             </div>
+</div>
          
     
 
