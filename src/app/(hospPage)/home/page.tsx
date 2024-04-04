2afc91cb23b7d6aed140f08b7687b83c89aeaba3
htandon16 <122512848+htandon16@users.noreply.github.com>
2024-04-05 01:23:10 +0530

    Update page.tsx

--- src/app/(hospPage)/home/page.tsx
+++ src/app/(hospPage)/home/page.tsx
@@ -65,20 +65,18 @@ const HomePage = () => {
 };
 
 const AyurvedaTreatmentSection = () => {
-  const sliderItems = [
-    { image: "/images/ayd_treate1.jpg", text: "Abhyangam", link: "https://naiminathayurveda.org" },
-    { image: "/images/ayd_treate2.jpg", text: "Shirodhara", link: "/shirodhara" },
-    { image: "/images/ayd_treate3.jpg", text: "Kashayadhara", link: "/kashayadhara" },
-    { image: "/images/ayd_treate4.jpg", text: "Greeva Basti", link: "/greeva-basti" },
-    { image: "/images/ayd_treate5.jpg", text: "Janu Basti", link: "/janu-basti" },
-    { image: "/images/ayd_treate6.jpg", text: "Kati Basti", link: "/kati-basti" },
-    { image: "/images/ayd_treate7.jpg", text: "Spine Basti", link: "/spine-basti" },
-    { image: "/images/ayd_treate8.jpg", text: "Kizhi", link: "/kizhi" },
+  const sliderItems: SliderItem[] = [
+    { image: "/images/ayd_treate1.jpg", text: "Abhyangam" },
+    { image: "/images/ayd_treate2.jpg", text: "Shirodhara" },
+    { image: "/images/ayd_treate3.jpg", text: "Kashayadhara" },
+    { image: "/images/ayd_treate4.jpg", text: "Greeva Basti" },
+    { image: "/images/ayd_treate5.jpg", text: "Janu Basti" },
+    { image: "/images/ayd_treate6.jpg", text: "Kati Basti" },
+    { image: "/images/ayd_treate7.jpg", text: "Spine Basti" },
+    { image: "/images/ayd_treate8.jpg", text: "Kizhi" },
   ];
-
   return (
-
-  <>
+    <>
       <h2 className="text-[28px] md:text-[34px] text-[#525252] font-semibold">
         Ayurvedic Treatments
       </h2>
@@ -86,16 +84,9 @@ const AyurvedaTreatmentSection = () => {
         List of Ayurvedic Treatments
       </p>
       <div className="w-[80%] md:w-[66%] my-[3%] md:mx-[20%] mx-[10%] items-center justify-center">
-<AyurvedaTreatmentImageSlider items={  
-    sliderItems.map((item, index) => (
-        <a key={index} href={item.link}>
-          <img src={item.image} alt={item.text} />
-        </a>
-      ))
-  }  /> 
+      <AyurvedaTreatmentImageSlider items={sliderItems} />
       </div>
     </>
-
   );
 };
 
