e6ec7b04f1d145117cc91299cb9032af1809691c
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-06 13:01:50 +0530

    Update page.tsx

--- src/app/(hospPage)/treatment/janu-basti/page.tsx
+++ src/app/(hospPage)/treatment/janu-basti/page.tsx
@@ -319,7 +319,7 @@ const JanuBastiPage = () => {
             Janu Basti procedure, putting me at ease with their professionalism
             and attentiveness.{" "}
           </p>
-          <footer>— jhanvi</footer>
+          <footer>— Sahil</footer>
         </div>
       </blockquote>
     </div>
