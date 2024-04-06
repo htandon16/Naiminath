f7455efe682dde8797d09244d0c63b044de10b59
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-06 12:24:39 +0530

    Update page.tsx

--- src/app/(hospPage)/treatment/abhyangam/page.tsx
+++ src/app/(hospPage)/treatment/abhyangam/page.tsx
@@ -163,7 +163,7 @@ const Abhyangam = () => {
       <blockquote className="testimonial">
         <div className="testimonial-image">
           <img
-            src="public/images/gallery/mohit.jpg"
+            src="/images/gallery/mohit.jpg"
             width={100}
             height={100}
           />
