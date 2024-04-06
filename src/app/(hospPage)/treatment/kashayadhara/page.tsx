fbb5b749b935fa403d75c22559a881e4e1b40e75
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-06 10:47:29 +0530

    Update page.tsx

--- src/app/(hospPage)/treatment/kashayadhara/page.tsx
+++ src/app/(hospPage)/treatment/kashayadhara/page.tsx
@@ -11,9 +11,9 @@ const KashydharaPage = () => {
       />
       <div>
        <div className="flex flex-wrap flex-row flex-4 gap-2 justify-center mx-2 md:mx-10 my-10">
-        <img className="rounded-md w-auto aspect-auto max-h-576" src="https://naiminathayurveda.org/wp-content/uploads/2023/09/jan_2014_Panchakarma_Hindi_960x636__copy.jpg"
+        <img className="rounded-md w-auto aspect-auto max-h-576" src="public/images/gallery/kashayadhara.jpg"
           sizes="(max-width: 640px) 100vw, 640px"
-          srcSet="https://naiminathayurveda.org/wp-content/uploads/2023/09/jan_2014_Panchakarma_Hindi_960x636__copy.jpg 960w, https://naiminathayurveda.org/wp-content/uploads/2023/09/jan_2014_Panchakarma_Hindi_960x636__copy-300x199.jpg 300w, https://naiminathayurveda.org/wp-content/uploads/2023/09/jan_2014_Panchakarma_Hindi_960x636__copy-768x509.jpg 768w"
+          srcSet="public/images/gallery/kashayadhara.jpg 960w, public/images/gallery/kashayadhara-300x199.jpg 300w, public/images/gallery/kashayadhara-768x509.jpg 768w"
           alt="Kashayadhara"
           width={640}
           height={424}
