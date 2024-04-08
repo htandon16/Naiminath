7fb55b9743ae4d8f2c1c0a3be391cacac5012f65
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-08 11:36:29 +0530

    Update ImageGallery.js

--- src/app/(hospPage)/hospital/opd/ImageGallery.js
+++ src/app/(hospPage)/hospital/opd/ImageGallery.js
@@ -21,15 +21,12 @@ const ImageGallery = () => {
     setEnlargedImage(null);
   };
 
-  // Function to handle keyboard navigation
   const handleKeyDown = (e) => {
     if (enlargedImage) {
       if (e.key === 'ArrowRight') {
-        // Move to the next image
         const nextIndex = (currentImageIndex + 1) % images.length;
         enlargeImage(images[nextIndex], nextIndex);
       } else if (e.key === 'ArrowLeft') {
-        // Move to the previous image
         const prevIndex = (currentImageIndex - 1 + images.length) % images.length;
         enlargeImage(images[prevIndex], prevIndex);
       }
@@ -57,15 +54,25 @@ const ImageGallery = () => {
         ))}
       </div>
       {enlargedImage && (
-        <div
-          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75"
-          onClick={closeEnlargedImage}
-        >
-          <img
-            className="max-h-full max-w-full"
-            src={enlargedImage}
-            alt="Enlarged Image"
-          />
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
+          <div className="relative">
+            <img
+              className="max-h-full max-w-full"
+              src={enlargedImage}
+              alt="Enlarged Image"
+            />
+            <div className="absolute top-1/2 -left-8 cursor-pointer" onClick={() => handleNavigation('left')}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-white hover:text-gray-300 transition duration-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"/>
+              </svg>
+            </div>
+            <div className="absolute top-1/2 -right-8 cursor-pointer" onClick={() => handleNavigation('right')}>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 text-white hover:text-gray-300 transition duration-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"/>
+              </svg>
+            </div>
+          </div>
+          <div className="absolute top-0 bottom-0 left-0 right-0" onClick={closeEnlargedImage}></div>
         </div>
       )}
     </div>
