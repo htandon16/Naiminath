8528adad9a159a3f78eaf40dc6faffd775b71561
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-12 15:29:09 +0530

    Update page.tsx

--- src/app/(hospPage)/home/page.tsx
+++ src/app/(hospPage)/home/page.tsx
@@ -59,7 +59,7 @@ const HomePage = () => {
           <PrivateBamsSection />
         </div>
 
-        <NaiminathAyurvedaSection />
+{/*          <NaiminathAyurvedaSection /> */}
       </div>
     </>
   );
