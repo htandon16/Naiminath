7c7c5dacbcd2e7572bceffbddf2a4b9053b8357e
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-13 14:40:12 +0530

    Update page.tsx

--- src/app/(hospPage)/home/page.tsx
+++ src/app/(hospPage)/home/page.tsx
@@ -62,11 +62,11 @@ const HomePage = () => {
           <h2 className="text-[26px] md:text-[32px] mt-5 mb-5 text-[#525252] font-semibold">
         Most Appreciated Services </h2>
 
-        <div className="flex flex-row flex-wrap flex-4 gap-2  justify-center">
+        <div className="flex flex-col sm:flex-row sm:flex-wrap justify-center gap-2">
 
-          <Image className="rounded-md w-auto h-60" src="/images/gallery/n1.jpg" alt="Patient 1"/>
-          <Image className="rounded-md w-auto h-60" src="/images/gallery/n2.jpg" alt="Patient 2"/>
-          <Image className="rounded-md w-auto h-60" src="/images/gallery/n3.jpg" alt="Patient 3"/>
+          <Image className="rounded-md w-auto h-60 sm:w-48 sm:h-auto" src="/images/gallery/n1.jpg" alt="Patient 1"/>
+          <Image className="rounded-md w-auto h-60 sm:w-48 sm:h-auto" src="/images/gallery/n2.jpg" alt="Patient 2"/>
+          <Image className="rounded-md w-auto h-60 sm:w-48 sm:h-auto" src="/images/gallery/n3.jpg" alt="Patient 3"/>
           <Image className="rounded-md w-auto h-60" src="/images/gallery/n4.jpg" alt="Patient 4"/>
           </div>
 {/*          <NaiminathAyurvedaSection /> */}
