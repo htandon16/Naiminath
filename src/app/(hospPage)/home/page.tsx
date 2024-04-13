ad6174ad03e28b70cdbde60b449749436fad4eb6
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-13 13:55:38 +0530

    Update page.tsx

--- src/app/(hospPage)/home/page.tsx
+++ src/app/(hospPage)/home/page.tsx
@@ -59,12 +59,15 @@ const HomePage = () => {
           <PrivateBamsSection />
         </div>
 
+          <h2 className="text-[28px] md:text-[34px] text-[#525252] font-semibold">
+        Most Appreciated Services </h2>
+
         <div className="flex flex-row flex-4 gap-2  justify-center">
 
-          <Image className="rounded-md w-60 aspect-auto max-h-60" src="/images/gallery/n1.jpg" alt="Patient 1"/>
-          <Image className="rounded-md w-60 aspect-auto maxh-h-60" src="/images/gallery/n2.jpg" alt="Patient 2"/>
-          <Image className="rounded-md w-60 aspect-square maxh-h-60" src="/images/gallery/n3.jpg" alt="Patient 3"/>
-          <Image className="rounded-md w-60 aspect-square maxh-h-60" src="/images/gallery/n4.jpg" alt="Patient 4"/>
+          <Image className="rounded-md w-60 aspect-auto h-auto" src="/images/gallery/n1.jpg" alt="Patient 1"/>
+          <Image className="rounded-md w-60 aspect-auto h-auto" src="/images/gallery/n2.jpg" alt="Patient 2"/>
+          <Image className="rounded-md w-60 aspect-auto h-60" src="/images/gallery/n3.jpg" alt="Patient 3"/>
+          <Image className="rounded-md w-60 aspect-auto h-60" src="/images/gallery/n4.jpg" alt="Patient 4"/>
           </div>
 
       
