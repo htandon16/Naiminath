64d2fb2f4259a342cb7847159d7f32c38332f26c
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-13 11:31:57 +0530

    Update page.tsx

--- src/app/(hospPage)/about/page.tsx
+++ src/app/(hospPage)/about/page.tsx
@@ -27,7 +27,7 @@ const AboutUs = () => {
         <div className="bg-[url('/images/About_naiminath_hospital-overlay (1).webp')] bg-center bg-cover
 bg-no-repeat  flex md:flex-row flex-col justify-center gap-6 mt-10 lg:px-20 sm:px-0 ">
 
-          <div className="flex justify-center">
+        
           <Image
             src="/images/About_naiminath_hospital.webp"
             className="w-[390px] h-[240px] md:h-[290px] rounded-[7px] shadow-md ring-1 ring-slate-100 sm:mx-5"
@@ -35,7 +35,7 @@ bg-no-repeat  flex md:flex-row flex-col justify-center gap-6 mt-10 lg:px-20 sm:p
             width={800}
             height={400}
           />
-            </div>
+           
            <div className="text-[#000000] text-[17px] px-4 flex-grow"> 
              {/*   <div className="w-[95%] md:w-[85%] h-full mt-3 md:mx-[10%] mx-[5%] py-[2%]">*/}
             <p className="mt-3">
