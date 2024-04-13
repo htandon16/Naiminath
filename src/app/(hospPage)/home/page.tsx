227388ce9f01784c71470cafe346bbe062c314eb
Abhinandan <146578461+abhinandankahol@users.noreply.github.com>
2024-04-13 14:02:44 +0530

    Update page.tsx

--- src/app/(hospPage)/home/page.tsx
+++ src/app/(hospPage)/home/page.tsx
@@ -64,8 +64,8 @@ const HomePage = () => {
 
         <div className="flex flex-row flex-4 gap-2  justify-center">
 
-          <Image className="rounded-md w-auto aspect-auto h-auto" src="/images/gallery/n1.jpg" alt="Patient 1"/>
-          <Image className="rounded-md w-auto aspect-auto h-auto" src="/images/gallery/n2.jpg" alt="Patient 2"/>
+          <Image className="rounded-md w-auto aspect-auto h-60" src="/images/gallery/n1.jpg" alt="Patient 1"/>
+          <Image className="rounded-md w-auto aspect-auto h-60" src="/images/gallery/n2.jpg" alt="Patient 2"/>
           <Image className="rounded-md w-auto  h-60" src="/images/gallery/n3.jpg" alt="Patient 3"/>
           <Image className="rounded-md w-auto h-60" src="/images/gallery/n4.jpg" alt="Patient 4"/>
           </div>
