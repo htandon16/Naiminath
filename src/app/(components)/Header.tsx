b7eae5751d5d2b40f5ae18e0b7df04d78e0c2654
pankaj-Ntechzy <pankaj@ntechzy.in>
2025-07-29 22:11:02 +0530

    permission letter updated

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -91,7 +91,7 @@ const Header = ({ imageSlider }: { imageSlider: boolean }) => {
 
 
             <div className="h-full border mx-3 border-[#a33a2e]"></div>
-            <Link href={"/doc/perm24.pdf"}>Permission Letter</Link>
+            <Link href={"/doc/Yearly_Permission.pdf"}>Permission Letter</Link>
           </div>
 
 
