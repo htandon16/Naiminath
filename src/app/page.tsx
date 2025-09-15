7312f748170a0ebd866ffea0f8a631aa37b3b629
kaushikntechzy <kaushik@ntechzy.in>
2025-09-15 10:52:08 +0530

    fix:Fixed app errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import BannerHeader from "./(components)/BannerHeader";
-import Footer from "./(components)/footer";
-import AboutHome from "./(components)/home/about/AboutHome";
-import ApplyHome from "./(components)/home/apply/ApplyHome";
-import BlogsHome from "./(components)/home/blogs/BlogsHome";
-import Brochure from "./(components)/home/Brochure";
-import WhyChoose from "./(components)/home/chooseUs/WhyChoose";
-import SocialPost from "./(components)/home/social/SocialPost";
-import TestimonialHome from "./(components)/home/testimonial/TestimonialHome";
-import TreatmentOpd from "./(components)/home/treatment/TreatmentOpd";
-import Youtube from "./(components)/home/youtube/Youtube";
+// import BannerHeader from "./(components)/BannerHeader";
+// import Footer from "./(components)/footer";
+// import AboutHome from "./(components)/home/about/AboutHome";
+// import ApplyHome from "./(components)/home/apply/ApplyHome";
+// import BlogsHome from "./(components)/home/blogs/BlogsHome";
+// import Brochure from "./(components)/home/Brochure";
+// import WhyChoose from "./(components)/home/chooseUs/WhyChoose";
+// import SocialPost from "./(components)/home/social/SocialPost";
+// import TestimonialHome from "./(components)/home/testimonial/TestimonialHome";
+// import TreatmentOpd from "./(components)/home/treatment/TreatmentOpd";
+// import Youtube from "./(components)/home/youtube/Youtube";
 import Home from "./(hospPage)/admissions/page";
-import HomePage from "./(hospPage)/home/page";
+// import HomePage from "./(hospPage)/home/page";
 // import Home from "./(hospPage)/home_new/page";
 // import Home from "./(hospPage)/admission-open/page";
 
